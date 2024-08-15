b22e541efe6eb8a790eaa5b0049de7587d0ab158
kemba <miles.welsh@live.com>
2024-08-15 12:10:39 -0600

    Updating IdlePlayerState.cs

--- src/scripts/IdlePlayerState.cs
+++ src/scripts/IdlePlayerState.cs
@@ -5,12 +5,21 @@ using Godot;
 public partial class IdlePlayerState : PlayerMovementState {
 
   [Export] public AnimationPlayer? AnimationPlayer;
+  [Export] public float SpeedDefault = 5.0f;
+  [Export] public float Acceleration = 0.1f;
+  [Export] public float Deceleration = 0.25f;
 
   public override void Enter() => AnimationPlayer?.Pause();
 
   public override void Update(float delta) {
-    if (Global.PlayerFpsController.Velocity.Length() > 0.0f && Global.PlayerFpsController.IsOnFloor()) {
-      EmitSignal(State.SignalName.Transition, "WalkingPlayerState");
+    if (PlayerFpsController != null)
+    {
+      PlayerFpsController.UpdateGravity(delta);
+      PlayerFpsController.UpdateInput(SpeedDefault, Acceleration, Deceleration);
+      PlayerFpsController.UpdateVelocity();
+      if (Global.PlayerFpsController.Velocity.Length() > 0.0f && Global.PlayerFpsController.IsOnFloor()) {
+        EmitSignal(State.SignalName.Transition, "WalkingPlayerState");
+      }
     }
   }
 }
