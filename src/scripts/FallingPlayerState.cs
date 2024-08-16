eda6a123694316d0640a0679ddebeca8e5c5f9ed
kemba <miles.welsh@live.com>
2024-08-16 12:51:45 -0600

    Updating FallingPlayerState.cs

--- src/scripts/FallingPlayerState.cs
+++ src/scripts/FallingPlayerState.cs
@@ -1,7 +1,51 @@
 using Godot;
-using System;
-using PrimeFPSGame.Scripts;
+namespace PrimeFPSGame.Scripts;
 
 public partial class FallingPlayerState : PlayerMovementState
 {
+    [Export] public AnimationPlayer? AnimationPlayer;
+    [Export] public float SpeedDefault = 5f;
+    [Export] public float Acceleration = 0.1f;
+    [Export] public float Deceleration = 0.25f;
+    [Export] public float DoubleJumpVelocity = 4.5f;
+
+    private bool _isDoubleJump;
+
+    public override void Enter(State currentState)
+    {
+        if (AnimationPlayer != null)
+        {
+            AnimationPlayer.Pause();
+        }
+    }
+
+    public override void Exit()
+    {
+        _isDoubleJump = false;
+    }
+
+    public override async void Update(float delta)
+    {
+        if (PlayerFpsController != null && AnimationPlayer != null)
+        {
+            PlayerFpsController.UpdateInput(SpeedDefault, Acceleration, Deceleration);
+            PlayerFpsController.UpdateVelocity();
+            PlayerFpsController.UpdateGravity(delta);
+            
+            if (Input.IsActionJustPressed("jump") && _isDoubleJump == false)
+            {
+                _isDoubleJump = true;
+                var tempPlayerVelocity = PlayerFpsController.Velocity;
+                tempPlayerVelocity.Y = DoubleJumpVelocity;
+                PlayerFpsController.Velocity = tempPlayerVelocity;
+            }
+            
+            if (PlayerFpsController.IsOnFloor())
+            {
+                AnimationPlayer.Play("JumpEnd");
+                await ToSignal(AnimationPlayer, AnimationPlayer.SignalName.AnimationFinished);
+                EmitSignal(State.SignalName.Transition, "IdlePlayerState");
+            }
+        }
+    }
 }
