e1d171dca5092d588010a8569fba9f980ab0381d
kemba <miles.welsh@live.com>
2024-08-17 16:48:47 -0600

    Adding WeaponsResource.cs

---
+++ src/scripts/WeaponsResource.cs
@@ -0,0 +1,15 @@
+using Godot;
+namespace PrimeFPSGame.Scripts;
+
+[GlobalClass]
+public partial class WeaponsResource : Resource
+{
+    
+    [Export] public string? Name;
+    [ExportCategory("Weapon Orientation")] 
+    [Export] public Vector3 Position;
+    [Export] public Vector3 Rotation;
+    [ExportCategory("Visual Settings")] 
+    [Export] public Mesh? Mesh;
+    [Export] public bool Shadow;
+}
