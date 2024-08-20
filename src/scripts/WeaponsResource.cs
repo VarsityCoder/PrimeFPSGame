ea0e67f96d1edc0cc577925760ea5897072e1dd8
kemba <miles.welsh@live.com>
2024-08-20 12:05:15 -0600

    Updating WeaponsResource.cs

--- src/scripts/WeaponsResource.cs
+++ src/scripts/WeaponsResource.cs
@@ -8,7 +8,26 @@ public partial class WeaponsResource : Resource
     [ExportCategory("Weapon Orientation")] 
     [Export] public Vector3 Position;
     [Export] public Vector3 Rotation;
+    [ExportCategory("Weapon Sway")] 
+    [Export] public Vector2 SwayMin = new Vector2(-20.0f, -20.0f);
+
+    [Export] public Vector2 SwayMax = new Vector2(20.0f, 20.0f);
+
+    [Export(PropertyHint.Range, "0,0.2,0.01")]
+    public float SwaySpeedPosition = 0.07f;
+
+    [Export(PropertyHint.Range, "0,0.2,0.01")]
+    public float SwaySpeedRotation = 0.1f;
+
+    [Export(PropertyHint.Range, "0,0.25,0.01")]
+    public float SwayAmountPosition = 0.1f;
+
+    [Export(PropertyHint.Range, "0,50,0.01")]
+    public float SwayAmountRotation = 30.0f;
+
     [ExportCategory("Visual Settings")] 
     [Export] public Mesh? Mesh;
     [Export] public bool Shadow;
+
+
 }
