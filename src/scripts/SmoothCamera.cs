cd9927fbd52e7b051999db38832276768ef5c8f6
kemba <miles.welsh@live.com>
2024-08-24 18:21:14 -0600

    Updating SmoothCamera.cs

--- src/scripts/SmoothCamera.cs
+++ src/scripts/SmoothCamera.cs
@@ -3,10 +3,10 @@ using Godot;
 
 namespace PrimeFPSGame.Scripts;
 public partial class SmoothCamera : Camera3D {
-  [Export] public float Speed = 44.0f;
+  [Export] private float _speed = 44.0f;
 
   public override void _PhysicsProcess(double delta) {
-    var weight = Math.Clamp(delta * Speed, 0.0, 1.0);
+    var weight = Math.Clamp(delta * _speed, 0.0, 1.0);
     GlobalTransform = GlobalTransform.InterpolateWith(GetParent<Node3D>().GlobalTransform, (float)weight);
     GlobalPosition = GetParent<Node3D>().GlobalPosition;
   }
