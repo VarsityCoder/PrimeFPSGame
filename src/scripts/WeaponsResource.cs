d388fe5f0a98cc9b03e60474ee662028fff93018
kemba <miles.welsh@live.com>
2024-08-24 18:21:47 -0600

    Updating WeaponsResource.cs

--- src/scripts/WeaponsResource.cs
+++ src/scripts/WeaponsResource.cs
@@ -4,34 +4,34 @@ namespace PrimeFPSGame.Scripts;
 [GlobalClass]
 public partial class WeaponsResource : Resource
 {
-    [Export] public string? Name;
+    [Export] protected internal string? Name;
     [ExportCategory("Weapon Orientation")] 
-    [Export] public Vector3 Position;
-    [Export] public Vector3 Rotation;
-    [Export] public Vector3 Scale;
+    [Export] protected internal Vector3 Position;
+    [Export] protected internal Vector3 Rotation;
+    [Export] protected internal Vector3 Scale;
     [ExportCategory("Weapon Sway")] 
-    [Export] public Vector2 SwayMin = new Vector2(-20.0f, -20.0f);
+    [Export] protected internal Vector2 SwayMin = new Vector2(-20.0f, -20.0f);
 
-    [Export] public Vector2 SwayMax = new Vector2(20.0f, 20.0f);
-
-    [Export(PropertyHint.Range, "0,0.2,0.01")]
-    public float SwaySpeedPosition = 0.07f;
+    [Export] protected internal Vector2 SwayMax = new Vector2(20.0f, 20.0f);
 
     [Export(PropertyHint.Range, "0,0.2,0.01")]
-    public float SwaySpeedRotation = 0.1f;
+    protected internal float SwaySpeedPosition = 0.07f;
+
+    [Export(PropertyHint.Range, "0,0.2,0.01")]
+    protected internal float SwaySpeedRotation = 0.1f;
 
     [Export(PropertyHint.Range, "0,0.25,0.01")]
-    public float SwayAmountPosition = 0.1f;
+    protected internal float SwayAmountPosition = 0.1f;
 
     [Export(PropertyHint.Range, "0,50,0.01")]
-    public float SwayAmountRotation = 30.0f;
+    protected internal float SwayAmountRotation = 30.0f;
 
-    [Export] public float IdleSwayAdjustment = 10.0f;
-    [Export] public float IdleSwayRotationStrength = 30.0f;
+    [Export] protected internal float IdleSwayAdjustment = 10.0f;
+    [Export] protected internal float IdleSwayRotationStrength = 30.0f;
 
     [ExportCategory("Visual Settings")] 
-    [Export] public Mesh? Mesh;
-    [Export] public bool Shadow;
+    [Export] protected internal Mesh? Mesh;
+    [Export] protected internal bool Shadow;
 
 
 }
