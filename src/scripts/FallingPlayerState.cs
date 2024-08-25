8386c752bee052b15aae92afddae035c3e7c0500
kemba <miles.welsh@live.com>
2024-08-24 18:19:43 -0600

    Updating FallingPlayerState.cs

--- src/scripts/FallingPlayerState.cs
+++ src/scripts/FallingPlayerState.cs
@@ -3,19 +3,19 @@ namespace PrimeFPSGame.Scripts;
 
 public partial class FallingPlayerState : PlayerMovementState
 {
-    [Export] public AnimationPlayer? AnimationPlayer;
-    [Export] public float SpeedDefault = 5f;
-    [Export] public float Acceleration = 0.1f;
-    [Export] public float Deceleration = 0.25f;
-    [Export] public float DoubleJumpVelocity = 4.5f;
+    [Export] private AnimationPlayer? _animationPlayer;
+    [Export] private float _speedDefault = 5f;
+    [Export] private float _acceleration = 0.1f;
+    [Export] private float _deceleration = 0.25f;
+    [Export] private float _doubleJumpVelocity = 4.5f;
 
     private bool _isDoubleJump;
 
     public override void Enter(State currentState)
     {
-        if (AnimationPlayer != null)
+        if (_animationPlayer != null)
         {
-            AnimationPlayer.Pause();
+            _animationPlayer.Pause();
         }
     }
 
@@ -26,9 +26,9 @@ public partial class FallingPlayerState : PlayerMovementState
 
     public override async void Update(float delta)
     {
-        if (PlayerFpsController != null && AnimationPlayer != null)
+        if (PlayerFpsController != null && _animationPlayer != null)
         {
-            PlayerFpsController.UpdateInput(SpeedDefault, Acceleration, Deceleration);
+            PlayerFpsController.UpdateInput(_speedDefault, _acceleration, _deceleration);
             PlayerFpsController.UpdateVelocity();
             PlayerFpsController.UpdateGravity(delta);
             
@@ -36,14 +36,14 @@ public partial class FallingPlayerState : PlayerMovementState
             {
                 _isDoubleJump = true;
                 var tempPlayerVelocity = PlayerFpsController.Velocity;
-                tempPlayerVelocity.Y = DoubleJumpVelocity;
+                tempPlayerVelocity.Y = _doubleJumpVelocity;
                 PlayerFpsController.Velocity = tempPlayerVelocity;
             }
             
             if (PlayerFpsController.IsOnFloor())
             {
-                AnimationPlayer.Play("JumpEnd");
-                await ToSignal(AnimationPlayer, AnimationPlayer.SignalName.AnimationFinished);
+                _animationPlayer.Play("JumpEnd");
+                await ToSignal(_animationPlayer, AnimationPlayer.SignalName.AnimationFinished);
                 EmitSignal(State.SignalName.Transition, "IdlePlayerState");
             }
         }
