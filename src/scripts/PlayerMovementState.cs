0fdf7f1a4997cfcde0c9a30f2fb20ca0624677b8
kemba <miles.welsh@live.com>
2024-08-26 14:06:31 -0600

    Updating PlayerMovementState.cs

--- src/scripts/PlayerMovementState.cs
+++ src/scripts/PlayerMovementState.cs
@@ -6,9 +6,11 @@ public partial class PlayerMovementState : State
 {
     protected PlayerFpsController? PlayerFpsController;
     private AnimationPlayer? _animationPlayer;
+    protected InitialWeapon? WeaponController;
     public override void _Ready()
     {
         Owner.Ready += () => PlayerFpsController = Owner as PlayerFpsController;
         _animationPlayer = PlayerFpsController?.AnimationPlayer;
+        WeaponController = PlayerFpsController?.WeaponController;
     }
 }
