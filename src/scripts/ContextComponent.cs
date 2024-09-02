a3ad1060f3d229ee8cfe76fc81a70de8ce6cc333
kemba <miles.welsh@live.com>
2024-09-02 13:30:17 -0600

    Updating ContextComponent.cs

--- src/scripts/ContextComponent.cs
+++ src/scripts/ContextComponent.cs
@@ -1,15 +1,42 @@
 using Godot;
-using System;
+namespace PrimeFPSGame.Scripts;
 
 public partial class ContextComponent : CenterContainer
 {
+	[Export] private TextureRect? _icon;
+	[Export] private Label? _context;
+	[Export] private Texture2D? _defaultIcon;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Global.UiContext = this;
+		Reset();	
+	}
+	private void Reset()
+	{
+		if (_icon != null && _context != null)
+		{
+			_icon.Texture = null;
+			_context.Text = "";
+		}
+
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	private void _updateIcon(Texture2D image, bool isOverride)
 	{
+		if (_icon != null)
+		{
+			_icon.Texture = isOverride ? image : _defaultIcon;
+		}
+
+	}
+
+	private void UpdateContext(string text)
+	{
+		if (_context != null)
+		{
+			_context.Text = text;
+		}
+
 	}
 }
