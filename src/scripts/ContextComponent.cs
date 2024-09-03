d611bd82ca616a5199d73134089da28e3ae8017d
kemba <miles.welsh@live.com>
2024-09-03 10:44:27 -0600

    Updating ContextComponent.cs

--- src/scripts/ContextComponent.cs
+++ src/scripts/ContextComponent.cs
@@ -6,15 +6,21 @@ public partial class ContextComponent : CenterContainer
 	[Export] private TextureRect? _icon;
 	[Export] private Label? _context;
 	[Export] private Texture2D? _defaultIcon;
+
+	private MessageBus _messageBus = new MessageBus(); 
+	
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Global.UiContext = this;
-		Reset();	
+		//TODO get messagebus working
+		_messageBus.Connect("InteractionFocused", new Callable(this, "Update"));
+		_messageBus.Connect("InteractionUnfocused", new Callable(this, "Reset"));
+		Reset();
 	}
 	public void Reset()
 	{
-		GD.Print("In Reset function");
 		if (_icon != null && _context != null)
 		{
 			_icon.Texture = null;
@@ -25,21 +31,38 @@ public partial class ContextComponent : CenterContainer
 
 	public void UpdateIcon(Texture2D image, bool isOverride)
 	{
-		GD.Print("In UpdateIcon function");
 		if (_icon != null)
 		{
 			_icon.Texture = isOverride ? image : _defaultIcon;
 		}
-
+	
 	}
-
+	
 	public void UpdateContent(string text)
 	{
-		GD.Print("In UpdateContent function");
 		if (_context != null)
 		{
 			_context.Text = text;
 		}
+	
+	}
 
+	public void Update(string text, Texture2D image, bool isOverride)
+	{
+		if (_context != null)
+		{
+			_context.Text = text;
+		}
+		if (_icon != null)
+		{
+			if (isOverride)
+			{
+				_icon.Texture = image;
+			}
+			else
+			{
+				_icon.Texture = _defaultIcon;
+			}
+		}
 	}
 }
