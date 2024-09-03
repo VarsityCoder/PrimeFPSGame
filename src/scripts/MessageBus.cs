ff6c845a0a243eacefcbe887c84f760b6762798a
kemba <miles.welsh@live.com>
2024-09-03 10:44:46 -0600

    Updating MessageBus.cs

--- src/scripts/MessageBus.cs
+++ src/scripts/MessageBus.cs
@@ -1,6 +1,13 @@
 using Godot;
-using System;
+namespace PrimeFPSGame.Scripts;
 
 public partial class MessageBus : Node
 {
+ //TODO implement this in the future not working right now
+ [Signal]
+ public delegate void InteractionFocusedEventHandler();
+
+ [Signal]
+ public delegate void InteractionUnfocusedEventHandler();
+
 }
