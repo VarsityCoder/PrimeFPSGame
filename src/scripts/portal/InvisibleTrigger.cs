f7e0cd1cadf9114b2e52e6a6e5f49968284f70e4
kemba <miles.welsh@live.com>
2024-09-16 08:31:26 -0600

    Updating InvisibleTrigger.cs. May not need this in future

--- src/scripts/portal/InvisibleTrigger.cs
+++ src/scripts/portal/InvisibleTrigger.cs
@@ -8,4 +8,34 @@ public partial class InvisibleTrigger : Area3D
     [Export] private bool _activate = true;
     [Export] private Array<Node> activables = new Array<Node>();
     
+
+    private void OnBodyEntered(Node3D body)
+    {
+        if (_activate && body.IsInGroup("player"))
+        {
+            foreach (var activable in activables)
+            {
+                if (true)
+                {
+                    
+                }
+                
+            }
+            
+        }
+    }
+
+    private void OnBodyExited(Node3D body)
+    {
+        if (!_activate && body.IsInGroup("player"))
+        {
+            foreach (var activable in activables)
+            {
+                if (true)
+                {
+                  
+                }
+            }
+        }
+    }
 }
