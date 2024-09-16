471d8867a5a320760d45daab65a456bf074a5638
kemba <miles.welsh@live.com>
2024-09-16 15:04:01 -0600

    Updating Portal.cs

--- src/scripts/portal/Portal.cs
+++ src/scripts/portal/Portal.cs
@@ -2,7 +2,32 @@ using Godot;
 
 namespace PrimeFPSGame.Scripts.Portal;
 
-public partial class Portal : Node
+public partial class Portal : Area3D
 {
-    
+    //private static bool _teleporting = false;
+    [Export] private int _verticalViewportResolution = 512;
+    private int id = -1;
+    private Tween? _tween;
+
+
+    public override void _Ready()
+    {
+        SetupViewportSize();
+    }
+
+    private void SetupViewportSize()
+    {
+        var viewportSize = GetViewport().GetWindow().Size;
+        var aspectRatio = (float)viewportSize.X  / viewportSize.Y;
+    }
+
+    private void OnBodyEntered(Node3D body)
+    {
+        
+    }
+
+    private void OnBodyExited(Node3D body)
+    {
+        
+    }
 }
