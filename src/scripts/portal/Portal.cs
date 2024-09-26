a991a28a69da4634f60e7f366f7b4fe4947fc2a7
kemba <miles.welsh@live.com>
2024-09-26 13:14:33 -0600

    Updating Portal.cs

--- src/scripts/portal/Portal.cs
+++ src/scripts/portal/Portal.cs
@@ -11,7 +11,7 @@ public partial class Portal : Area3D
     private Tween? _tween;
     public PortalCamera? Camera3D;
     private SubViewport? _subViewport;
-    private Portal? _exitPortal = null;
+    private Portal? _exitPortal;
     private ShaderMaterial? _material;
 
     public Portal? ExitPortal
@@ -66,63 +66,67 @@ public partial class Portal : Area3D
     private void OnBodyEntered(Node3D body)
     {           
         GD.Print("In on body entered");
-        if (!_teleporting && Global.PlayerFpsController.IsInGroup("player"))
+        if (Global.PlayerFpsController != null)
         {
-            GD.Print("In if statement");
-            _teleporting = true;
-            var h = Global.PlayerFpsController.GetNode<Node3D>("CameraController");
-            var c = h.GetNode<Camera3D>("Recoil/Camera3D");
-            var inForward = -GlobalTransform.Basis.Z;
-            if (_exitPortal != null)
+            if (!_teleporting && Global.PlayerFpsController.IsInGroup("player"))
             {
-                GD.Print("We are in _exitportal if");
-                var outForward = -_exitPortal.GlobalTransform.Basis.Z;
-                var inUp = GlobalTransform.Basis.Y;
-                var outUp = _exitPortal.GlobalTransform.Basis.Y;
-                var initCamRot = c.Rotation;
-                var initYaw = c.GlobalTransform.Basis.Z.SignedAngleTo(inForward, inUp);
-                var outIsHorizontal = outForward.Dot(Vector3.Up) > 0.9;
+                
+                GD.Print("In if statement");
+                _teleporting = true;
+                var h = Global.PlayerFpsController.GetNode<Node3D>("CameraController");
+                var c = h.GetNode<Camera3D>("Recoil/Camera3D");
+                var inForward = -GlobalTransform.Basis.Z;
+                if (_exitPortal != null)
+                {
+                    GD.Print("We are in _exitportal if");
+                    var outForward = -_exitPortal.GlobalTransform.Basis.Z;
+                    var inUp = GlobalTransform.Basis.Y;
+                    var outUp = _exitPortal.GlobalTransform.Basis.Y;
+                    var initCamRot = c.Rotation;
+                    var initYaw = c.GlobalTransform.Basis.Z.SignedAngleTo(inForward, inUp);
+                    var outIsHorizontal = outForward.Dot(Vector3.Up) > 0.9;
 
-                var posDiff = Global.PlayerFpsController.GlobalPosition - GlobalPosition;
-                Global.PlayerFpsController.GlobalRotation = _exitPortal.GlobalPosition + posDiff;
+                    var posDiff = Global.PlayerFpsController.GlobalPosition - GlobalPosition;
+                    Global.PlayerFpsController.GlobalRotation = _exitPortal.GlobalPosition + posDiff;
             
-                c.Rotation = Vector3.Zero;
-                h.LookAt(  Global.PlayerFpsController.GlobalPosition + outForward, outUp);
-                var tempVector3 = h.Rotation;
-                tempVector3.Y -= initYaw;
-                h.Rotation = tempVector3;
-                var pitch = Vector3.Up.SignedAngleTo(h.GlobalTransform.Basis.Y, h.GlobalTransform.Basis.X);
-                h.RotateX(pitch);
-                c.Rotation = initCamRot;
-                c.Rotation += Vector3.Right * inUp.SignedAngleTo(outUp, _exitPortal.GlobalTransform.Basis.X);
+                    c.Rotation = Vector3.Zero;
+                    h.LookAt(  Global.PlayerFpsController.GlobalPosition + outForward, outUp);
+                    var tempVector3 = h.Rotation;
+                    tempVector3.Y -= initYaw;
+                    h.Rotation = tempVector3;
+                    var pitch = Vector3.Up.SignedAngleTo(h.GlobalTransform.Basis.Y, h.GlobalTransform.Basis.X);
+                    h.RotateX(pitch);
+                    c.Rotation = initCamRot;
+                    c.Rotation += Vector3.Right * inUp.SignedAngleTo(outUp, _exitPortal.GlobalTransform.Basis.X);
             
-                if(!outIsHorizontal)
-                {
-                    if (c.RotationDegrees.X < -100.0)
+                    if(!outIsHorizontal)
                     {
-                        var tempVector3CRotation = c.Rotation;
-                        tempVector3CRotation.X += Single.Pi;
-                        c.Rotation = tempVector3CRotation;
-                    } 
-                    else if (c.RotationDegrees.X > 100.0)
-                    {
-                        var tempVector3CRotation = c.Rotation;
-                        tempVector3CRotation.X -= Single.Pi;
-                        c.Rotation = tempVector3CRotation;
+                        if (c.RotationDegrees.X < -100.0)
+                        {
+                            var tempVector3CRotation = c.Rotation;
+                            tempVector3CRotation.X += Single.Pi;
+                            c.Rotation = tempVector3CRotation;
+                        } 
+                        else if (c.RotationDegrees.X > 100.0)
+                        {
+                            var tempVector3CRotation = c.Rotation;
+                            tempVector3CRotation.X -= Single.Pi;
+                            c.Rotation = tempVector3CRotation;
+                        }
+                        var p = new Plane(-outForward);
+                        var velXz = p.Project(Global.PlayerFpsController.Velocity);
+                        var velY = Global.PlayerFpsController.Velocity.Project(inUp);
+                        Global.PlayerFpsController.Velocity = velXz.Length() * -c.GlobalTransform.Basis.Z + velY * outUp;
                     }
-                    var p = new Plane(-outForward);
-                    var velXz = p.Project(Global.PlayerFpsController.Velocity);
-                    var velY = Global.PlayerFpsController.Velocity.Project(inUp);
-                    Global.PlayerFpsController.Velocity = velXz.Length() * -c.GlobalTransform.Basis.Z + velY * outUp;
-                }
-                else
-                {
-                    Global.PlayerFpsController.Velocity = outForward * Global.PlayerFpsController.Velocity.Length();
-                }
-                if (outForward.Dot(Vector3.Up) > 0.9)
-                {
-                    Global.PlayerFpsController.Velocity += Vector3.Up * 0.1f;
-                    Global.PlayerFpsController.GlobalTranslate(Vector3.Up * 0.3f);
+                    else
+                    {
+                        Global.PlayerFpsController.Velocity = outForward * Global.PlayerFpsController.Velocity.Length();
+                    }
+                    if (outForward.Dot(Vector3.Up) > 0.9)
+                    {
+                        Global.PlayerFpsController.Velocity += Vector3.Up * 0.1f;
+                        Global.PlayerFpsController.GlobalTranslate(Vector3.Up * 0.3f);
+                    }
                 }
             }
         }
