00eeb0ba8dc7b538af916141c75301063c356ac6
kemba <miles.welsh@live.com>
2024-09-26 04:37:52 -0600

    Adding StencilBlocker.cs

---
+++ src/scripts/portal/StencilBlocker.cs
@@ -0,0 +1,10 @@
+using Godot;
+namespace PrimeFPSGame.Scripts.Portal;
+
+public partial class StencilBlocker : MeshInstance3D
+{
+    public override void _Ready()
+    {
+        Visible = true;
+    }
+}
