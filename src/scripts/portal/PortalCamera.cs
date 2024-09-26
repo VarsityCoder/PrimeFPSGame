8e3b5b4aa99c9a8b7b55785a45cbc17eb212c052
kemba <miles.welsh@live.com>
2024-09-26 04:37:44 -0600

    Updating PortalCamera.cs

--- src/scripts/portal/PortalCamera.cs
+++ src/scripts/portal/PortalCamera.cs
@@ -9,41 +9,52 @@ public partial class PortalCamera : Camera3D
     [Export] private float _exitNearSubtract = 0.05f;
     private Portal? _portal;
     private Aabb _meshAabb;
-    private Portal? _exitPortal;
+    public Portal? ExitPortal;
 
     public override void _Ready()
     {
-        SetProcess(false);
+        GD.Print("Portal Camera exists");
         _portal = GetParent().GetParent() as Portal;
-        if (_portal != null) _meshAabb = _portal.GetNode<MeshInstance3D>("Mesh").Mesh.GetAabb();
+        if (_portal != null) _meshAabb = _portal.GetNode<MeshInstance3D>("PortalMesh").Mesh.GetAabb();
     }
 
     public override void _Process(double delta)
     {
-        if (_portal != null && _exitPortal != null && _exitPortal.Camera3D != null && Global.PlayerFpsController != null 
+        if (ExitPortal != null)
+        {
+            GD.Print("Portal is not null");
+        }
+        else
+        {
+            GD.Print("Portal is null");
+        }
+        if (_portal != null && ExitPortal != null && ExitPortal.Camera3D != null && Global.PlayerFpsController != null 
             && Global.PlayerFpsController.CameraController != null)
         {
+
+            GD.Print("Exit portal transform is working");
             var t = _portal.GlobalTransform.AffineInverse() * Global.PlayerFpsController.CameraController.GlobalTransform;
             t = t.Rotated(Vector3.Up, Single.Pi);
-            t = _exitPortal.Camera3D.GlobalTransform * t;
-            _exitPortal.Camera3D.GlobalTransform = t;
+            t = ExitPortal.Camera3D.GlobalTransform * t;
+            ExitPortal.Camera3D.GlobalTransform = t;
         }
 
-        if (_exitPortal != null && _exitPortal.Camera3D != null)
+        if (ExitPortal != null && ExitPortal.Camera3D != null)
         {
-            var corner1 = _exitPortal.ToGlobal(new Vector3(_meshAabb.Position.X, _meshAabb.Position.Y, 0) * ExitScale);
-            var corner2 = _exitPortal.ToGlobal(new Vector3(_meshAabb.Position.X + _meshAabb.Size.X, _meshAabb.Position.Y, 0) * ExitScale);
-            var corner3 = _exitPortal.ToGlobal(new Vector3(_meshAabb.Position.X + _meshAabb.Size.X, _meshAabb.Position.Y + _meshAabb.Size.Y, 0) * ExitScale);
-            var corner4 = _exitPortal.ToGlobal(new Vector3(_meshAabb.Position.X, _meshAabb.Position.Y + _meshAabb.Size.Y, 0) * ExitScale);
+            GD.Print("Exit portal corner is working");
+            var corner1 = ExitPortal.ToGlobal(new Vector3(_meshAabb.Position.X, _meshAabb.Position.Y, 0) * ExitScale);
+            var corner2 = ExitPortal.ToGlobal(new Vector3(_meshAabb.Position.X + _meshAabb.Size.X, _meshAabb.Position.Y, 0) * ExitScale);
+            var corner3 = ExitPortal.ToGlobal(new Vector3(_meshAabb.Position.X + _meshAabb.Size.X, _meshAabb.Position.Y + _meshAabb.Size.Y, 0) * ExitScale);
+            var corner4 = ExitPortal.ToGlobal(new Vector3(_meshAabb.Position.X, _meshAabb.Position.Y + _meshAabb.Size.Y, 0) * ExitScale);
 
-            var cameraForward = -_exitPortal.GlobalTransform.Basis.Z.Normalized();
+            var cameraForward = -ExitPortal.GlobalTransform.Basis.Z.Normalized();
 
-            var d1 = (corner1 - _exitPortal.Camera3D.GlobalPosition).Dot(cameraForward);
-            var d2 = (corner2 - _exitPortal.Camera3D.GlobalPosition).Dot(cameraForward);
-            var d3 = (corner3 - _exitPortal.Camera3D.GlobalPosition).Dot(cameraForward);
-            var d4 = (corner4 - _exitPortal.Camera3D.GlobalPosition).Dot(cameraForward);
+            var d1 = (corner1 - ExitPortal.Camera3D.GlobalPosition).Dot(cameraForward);
+            var d2 = (corner2 - ExitPortal.Camera3D.GlobalPosition).Dot(cameraForward);
+            var d3 = (corner3 - ExitPortal.Camera3D.GlobalPosition).Dot(cameraForward);
+            var d4 = (corner4 - ExitPortal.Camera3D.GlobalPosition).Dot(cameraForward);
         
-            _exitPortal.Camera3D.Near = float.Max(ExitCameraNearMin, float.Min(float.Min(d1,d2), float.Min(d3,d4)) - _exitNearSubtract);
+            ExitPortal.Camera3D.Near = float.Max(ExitCameraNearMin, float.Min(float.Min(d1,d2), float.Min(d3,d4)) - _exitNearSubtract);
         }
     }
 }
